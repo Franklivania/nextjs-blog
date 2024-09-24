5d83a42aa5e213539614e62892b6d421a2f65158
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2024-09-24 18:27:35 +0100

    fix: add projectId to sanity

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url"
 
 export const client = createClient ({
-  projectId: process.env.NEXT_PROJECT_ID,
+  projectId: process.env.NEXT_PROJECT_ID || "vwv5f7l4",
   apiVersion: "2024-01-01",
   dataset: "production",
   useCdn: false
